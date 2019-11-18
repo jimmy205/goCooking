63b5534fe01e1cccbf84f2ce20b876a6e4471787
hsing_liu <hsing_liu@mail.chungyo.net>
2019-11-18 16:00:06 +0800

    class 0.2 version 3

--- dropBall/drop_ball.go
+++ dropBall/drop_ball.go
@@ -2,23 +2,35 @@ package dropball
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
 type ball struct {
-	to string
+	ID int
 }
 
 // Heaven 天堂
 type Heaven struct {
 	msgChan chan string
-	personA *person
-	personB *person
-	personC *person
+	personA *personA
+	personB *personB
+	personC *personC
 }
 
-type person struct {
+type personA struct {
 	handChan chan *ball
+	heaven   *Heaven
+}
+
+type personB struct {
+	handChan chan *ball
+	heaven   *Heaven
+}
+
+type personC struct {
+	handChan chan *ball
+	heaven   *Heaven
 }
 
 // Lab 控制中心啟動
@@ -26,75 +38,104 @@ func (h *Heaven) Lab() {
 
 	h.msgChan = make(chan string)
 
-	h.personA = &person{
+	h.personA = &personA{
 		handChan: make(chan *ball, 5),
 	}
-	h.personB = &person{
+	h.personB = &personB{
 		handChan: make(chan *ball, 2),
 	}
-	h.personC = &person{
+	h.personC = &personC{
 		handChan: make(chan *ball, 2),
 	}
 
-	go h.getBall()
+	h.personA.heaven = h
+	h.personB.heaven = h
+	h.personC.heaven = h
 
-	for {
-		time.Sleep(time.Second)
+	go h.personA.passBall()
+	go h.personB.passBall()
+	go h.personC.handleBall()
+	go h.waitingCall()
+
+	for i := 1; i <= 1000; i++ {
 
 		ball := &ball{
-			to: "B",
+			ID: i,
 		}
 
 		select {
 		case h.personA.handChan <- ball:
-			fmt.Println("A got ball,to B")
+			log.Println("A 拿到球 -> ", ball.ID)
 		default:
-			fmt.Println("A miss ball ... ?")
+			log.Println("A 沒手了")
 		}
 
+		time.Sleep(time.Second)
 	}
 
 }
 
-func (h *Heaven) getBall() {
+func (h *Heaven) waitingCall() {
 	for {
-
-		msg := &ball{}
 		select {
-		case msg = <-h.personA.handChan:
-		case msg = <-h.personB.handChan:
-			msg.to = "C"
-		case msg = <-h.personC.handChan:
-			fmt.Println("c got Ball,handle ball")
-			for i := 0; i < 3; i++ {
-				fmt.Println("handling -> ", i)
-				time.Sleep(time.Second)
+		case msg := <-h.msgChan:
+			switch msg {
+			case "B":
+				log.Println("B call C")
+			case "C":
+
+				for i := 0; i < len(h.personC.handChan); i++ {
+					fmt.Println("c Drop Ball -> ", <-h.personC.handChan)
+					time.Sleep(time.Millisecond * 10)
+				}
+
 			}
 		}
-
-		h.handleBall(msg)
 	}
 }
 
-func (h *Heaven) handleBall(msg *ball) {
+func (a *personA) passBall() {
 
-	switch msg.to {
-	case "B":
-		select {
-		case h.personB.handChan <- msg:
-			fmt.Println("B got ball,to C")
-		default:
-			fmt.Println("B miss ball,A call god")
-		}
-	case "C":
-		select {
-		case h.personC.handChan <- msg:
-			fmt.Println("C got ball,Ready Handle it")
+	for elme := range a.handChan {
 
+		select {
+		case a.heaven.personB.handChan <- elme:
+			log.Println("B 拿到球 -> ", elme.ID)
 		default:
-			fmt.Println("C miss ball,B call god")
+			log.Println("B 沒手了")
+			a.heaven.msgChan <- "B"
 		}
 
 	}
 
+}
+
+func (b *personB) passBall() {
+	for elme := range b.handChan {
+		select {
+		case b.heaven.personC.handChan <- elme:
+			log.Println("C 拿到球 -> ", elme.ID)
+
+		default:
+		Loop:
+			for {
+				select {
+				case b.heaven.personC.handChan <- elme:
+					log.Println("C 拿到球")
+					break Loop
+				default:
+					log.Println("call C")
+					b.heaven.msgChan <- "C"
+					time.Sleep(time.Second / 2)
+				}
+			}
+		}
+	}
+}
+
+func (c *personC) handleBall() {
+	for elme := range c.handChan {
+		time.Sleep(time.Second * 2)
+		log.Println("C 把球處理掉了 -> ", elme.ID)
+	}
 }

56ba2a0496aa35666f212b8895618159cf377474
hsing_liu <hsing_liu@mail.chungyo.net>
2019-12-05 18:00:13 +0800

    增加註解

--- mineGround/mine/mine_handler.go
+++ mineGround/mine/mine_handler.go
@@ -1,11 +1,11 @@
 package mine
 
-// GoldHandler 通知金礦產生
+// GoldHandler 礦場提供金礦產生的Func
 func (g *Ground) GoldHandler(fn func(string)) {
 	g.gGoldHandler = fn
 }
 
-// RockHandler 通知石頭產生
+// RockHandler 礦場提供石頭產生的Func
 func (g *Ground) RockHandler(fn func(string)) {
 	g.gRockHandler = fn
 }
